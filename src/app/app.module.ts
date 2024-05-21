b9d06553bd8def15f88f82ab76f3801ee116f726
Khouloud Mhamdi <121306018+Khouloud-Mhamdi@users.noreply.github.com>
2024-05-21 10:47:50 +0100

    changement front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-=======
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
->>>>>>> origin/Sarra
+
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -17,48 +14,47 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
-<<<<<<< HEAD
+
 
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { RegisterCentreCampingComponent } from './pages/register-centre-camping/register-centre-camping.component';
 
-import { ProduitComponent } from './pages/produit/produit.component';
-import { AddProduitComponent } from './pages/add-produit/add-produit.component';
-import { ChartComponent } from './pages/chart/chart.component';
-import { PayementDoneComponent } from './pages/payement-done/payement-done.component';
+
 import {MatStepperModule} from '@angular/material/stepper';
-import { DashbordAdminComponent } from './pages/dashbord-admin/dashbord-admin.component';
+
 import { MatTabsModule } from '@angular/material/tabs';
 
 import { MatDialog } from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { UpdateResComponent } from './components/Reservation/liste-reservation/update-res/update-res.component';
-import { ListeReservationComponent } from './components/Reservation/liste-reservation/liste-reservation.component';
-import { ChartPipeComponent } from './components/Reservation/chart-pipe/chart-pipe.component';
-import { FilterPipe } from './filter-reservation.pipe';
+import {MatCardModule} from '@angular/material/card';
 
-=======
-import { CampsiteComponent } from './pages/campsite/campsite.component';
-import { ListCampsitesComponent } from './pages/list-campsites/list-campsites.component';
+
 import { ClickimgDirective } from './clickimg.directive';
-import { AllDetailsCampsiteComponent } from './pages/all-details-campsite/all-details-campsite.component';
-import {MatStepperModule} from "@angular/material/stepper";
+
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from "@angular/material/icon";
-import {MatCardModule} from "@angular/material/card";
 import {MatTableModule} from "@angular/material/table";
 import {MatSortModule} from "@angular/material/sort";
->>>>>>> origin/Sarra
+import { ChartPipeComponent } from './components/Reservation/chart-pipe/chart-pipe.component';
+import { ListeReservationComponent } from './components/Reservation/liste-reservation/liste-reservation.component';
+import { UpdateResComponent } from './components/Reservation/liste-reservation/update-res/update-res.component';
+import { FilterPipe } from './filter-reservation.pipe';
+import { AddProduitComponent } from './pages/add-produit/add-produit.component';
+import { AllDetailsCampsiteComponent } from './pages/all-details-campsite/all-details-campsite.component';
+import { CampsiteComponent } from './pages/campsite/campsite.component';
+import { DashbordAdminComponent } from './pages/dashbord-admin/dashbord-admin.component';
+import { ListCampsitesComponent } from './pages/list-campsites/list-campsites.component';
+import { PayementDoneComponent } from './pages/payement-done/payement-done.component';
+import { ProduitComponent } from './pages/produit/produit.component';
+import { RegisterCentreCampingComponent } from './pages/register-centre-camping/register-centre-camping.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   imports: [
@@ -71,12 +67,9 @@ import {MatSortModule} from "@angular/material/sort";
     RouterModule,
     AppRoutingModule,
     ReactiveFormsModule,
-<<<<<<< HEAD
-
     MatStepperModule,
     MatFormFieldModule,
     MatButtonModule,
-
     MatTabsModule,
     MatDialogModule,
     MatButtonModule,
@@ -86,9 +79,8 @@ import {MatSortModule} from "@angular/material/sort";
     MatInputModule,
     FormsModule,
     MatToolbarModule,
-    MatSlideToggleModule
-   
-=======
+    MatSlideToggleModule,
+    MatCardModule,
     MatStepperModule,
     MatFormFieldModule,
     MatButtonModule,
@@ -96,35 +88,26 @@ import {MatSortModule} from "@angular/material/sort";
     MatCardModule,
     MatTableModule,
     MatSortModule
->>>>>>> origin/Sarra
   ],
   declarations: [
     AppComponent,
     AdminLayoutComponent,
     AuthLayoutComponent,
-<<<<<<< HEAD
     RegisterCentreCampingComponent,
     ProduitComponent,
     AddProduitComponent,
-    ChartComponent,
     PayementDoneComponent,
     DashbordAdminComponent,
-    
     ListeReservationComponent,
     UpdateResComponent,
     UpdateResComponent,
     FilterPipe,
     ChartPipeComponent,
-  
-   
-   
-=======
     CampsiteComponent,
     ListCampsitesComponent,
     ClickimgDirective,
     AllDetailsCampsiteComponent,
 
->>>>>>> origin/Sarra
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -6,21 +6,19 @@ import { IconsComponent } from '../../pages/icons/icons.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
-<<<<<<< HEAD
-import { ListUsersComponent } from 'src/app/pages/list-users/list-users.component';
-import { ProduitComponent } from 'src/app/pages/produit/produit.component';
-import { AddProduitComponent } from 'src/app/pages/add-produit/add-produit.component';
-import { ChartComponent } from 'src/app/pages/chart/chart.component';
-import { ConfirmComponent } from 'src/app/components/dialog/confirm/confirm.component';
-import { PayementDoneComponent } from 'src/app/pages/payement-done/payement-done.component';
-import { ListeReservationComponent } from 'src/app/components/Reservation/liste-reservation/liste-reservation.component';
+
 import { Routes } from '@angular/router';
 
-=======
 import {CampsiteComponent} from "../../pages/campsite/campsite.component";
 import {ListCampsitesComponent} from "../../pages/list-campsites/list-campsites.component";
 import {AllDetailsCampsiteComponent} from "../../pages/all-details-campsite/all-details-campsite.component";
->>>>>>> origin/Sarra
+import { ProduitComponent } from 'src/app/pages/produit/produit.component';
+import { ConfirmComponent } from 'src/app/components/dialog/confirm/confirm.component';
+import { PayementDoneComponent } from 'src/app/pages/payement-done/payement-done.component';
+import { AddProduitComponent } from 'src/app/pages/add-produit/add-produit.component';
+import { ListeReservationComponent } from 'src/app/components/Reservation/liste-reservation/liste-reservation.component';
+import { ListUsersComponent } from 'src/app/pages/list-users/list-users.component';
+import { ChartComponent } from 'src/app/pages/chart/chart.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'produit',      component: ProduitComponent },
@@ -31,19 +29,15 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'addproduit',      component: AddProduitComponent },
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'reservation',      component: ListeReservationComponent },
-
     { path: 'user-profile',   component: UserProfileComponent },
     { path: 'tables',         component: TablesComponent },
     { path: 'icons',          component: IconsComponent },
     { path: 'maps',           component: MapsComponent },
-<<<<<<< HEAD
     { path : 'ListUser',      component :ListUsersComponent},
     { path: 'dashboardAdmin',      component: DashbordAdminComponent },
-=======
-  { path: 'campsite',           component: CampsiteComponent },
-  { path: 'listcampsite',           component: ListCampsitesComponent },
-  { path: 'detaillistcampsite/:id', component: AllDetailsCampsiteComponent }
+    { path: 'campsite',           component: CampsiteComponent },
+    { path: 'listcampsite',           component: ListCampsitesComponent },
+    { path: 'detaillistcampsite/:id', component: AllDetailsCampsiteComponent }
 
 
->>>>>>> origin/Sarra
 ];
